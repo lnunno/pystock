f6c724ddff6b9ccf644762ba3a0612b6e600d95e
Lucas <lucasnunno@gmail.com>
2014-04-21 11:57:39 -0600

    Update driver.py
    Updated documentation.

--- src/pystock/driver.py
+++ src/pystock/driver.py
@@ -19,6 +19,10 @@ from pystock.Stock import Stock
 ticker_column = 4
 
 def load_stock_csv_file(file_path):
+    '''
+    Load the given stock csv file and create a pandas DataFrame representation of the file.
+    @return: A DataFrame of stock price data.
+    '''
     abspath = os.path.abspath(file_path)
     print 'Loading %s...' % (abspath)
     def line_filterer(stock_file):
@@ -34,7 +38,14 @@ def load_stock_csv_file(file_path):
             
     def date_conv(date_str):
         '''
-        @return: A DataFrame of stock price data.
+        Convert the date from a string to a numpy datetime64 object.
+        
+        Expects the date to be formatted as %Y/%m/%d or else it will return None and print a warning.
+        
+        These are examples of valid input:
+        '2004/01/30'
+        '2014/02/23'
+        etc.
         '''
         try:
             dt = datetime.strptime(date_str, '%Y/%m/%d')
@@ -66,6 +77,10 @@ def random_transaction(broker, date):
         return
 
 def load_all_stock_files(root_dir, file_name_ls):
+    '''
+    Load all the stock data files from the given root directory. 
+    @return: A concatenated master DataFrame containing the data from all the given csv files.
+    '''
     data_frame_ls = []
     for file_name in file_name_ls:
         full_path = os.path.join(root_dir, file_name)
@@ -75,6 +90,9 @@ def load_all_stock_files(root_dir, file_name_ls):
     return full_df 
 
 def create_arg_parser():
+    '''
+    Create an argument for running the script with user defined values.
+    '''
     parser = ArgumentParser()
     parser.add_argument('--loadStockDF', help='The path to a pickled DataFrame of stock data to load.', action='store', dest='load_pkl_path', default='')
     parser.add_argument('--saveStockDF', help='The path to save a pickled version of the stock data to.', action='store', dest='save_pkl_path', default='')
